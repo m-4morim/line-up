155c1998256697bb52231751873835dc18a27185
m-4morim <m.4morim@outlook.com>
2023-03-22 05:59:44 -0300

    :bricks: build: Criando o footer

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -4,10 +4,14 @@ export const Footer = () => {
     // JSX
     return (
         <footer className="footer">
-            <div>
-                <h6>Criado por Marcos Amorim - </h6>
-                <img src="/imagens/footer.jpg" alt="Bola oficial da NBA" />
+            <h4>Criado por @Marcos Amorim</h4>
+            <div className="footer-link">
+                <img src="/imagens/github-logo.jpg" />
+                <a href="https://github.com/m-4morim/line-up">
+                    <h4>GitHub</h4>
+                </a>
             </div>
+            <img className="footer-img" src="/imagens/footer.jpg" alt="Bola oficial da NBA" />
         </footer>
     )
 }
