13bc80e41b753767ebc608d38c2fd20529131c87
m-4morim <m.4morim@outlook.com>
2023-03-24 19:49:05 -0300

    :dizzy: build: Removendo Arrays vazias

--- src/componentes/Posicao/index.js
+++ src/componentes/Posicao/index.js
@@ -6,12 +6,13 @@ const Posicao = (props) => {
     const css = { backgroundColor: props.corSecundaria }
 
     return (
-        <section className='posicao' style={css}>
+        (props.jogadores.length > 0) ? <section className='posicao' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='jogadores'>
                 {props.jogadores.map( jogador => <Jogador nome={jogador.nome} equipe={jogador.equipe} imagem={jogador.imagem}/> )}
             </div>
         </section>
+        : ''
     )
 }
 
