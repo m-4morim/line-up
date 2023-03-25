46a58e2b655e6814a177c4d56b76ead2bb943265
m-4morim <m.4morim@outlook.com>
2023-03-25 14:10:42 -0300

    :bug: fix: Adicionando debugger

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,13 +3,18 @@ import './Banner.css'
 export const Banner = () => {
     // JSX
     return (
-        <header>
-            <div className="banner">
-                <img src="/imagens/banner.jpg" alt="Disputa por uma jogada na NBA" />
-            </div>
-            <div className="banner-logo">
-                <img src="/imagens/logo.jpg" alt="Logo da NBA" />
-            </div>
-        </header>
+        //<Fragment>
+        <>
+            <header>
+                <div className="banner">
+                    <img src="/imagens/banner.jpg" alt="Disputa por uma jogada na NBA" />
+                </div>
+                <div className="banner-logo">
+                    <img src="/imagens/logo.jpg" alt="Logo da NBA" />
+                </div>
+            </header>
+        </>
+            //<h1></h1>
+        //</Fragment>
     )
 }
