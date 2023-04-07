471e990a5384ab9ba9ec171271a0c0e6406a2425
m-4morim <m.4morim@outlook.com>
2023-04-06 21:29:27 -0300

    :sparkles: feat: Criando função aoDeletar

--- src/componentes/Posicao/index.js
+++ src/componentes/Posicao/index.js
@@ -4,12 +4,14 @@ import './Posicao.css'
 const Posicao = (props) => {
 
     const css = { backgroundColor: props.corSecundaria }
-
     return (
         (props.jogadores.length > 0) ? <section className='posicao' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='jogadores'>
-                {props.jogadores.map( jogador => <Jogador corDeFundo={props.corPrimaria} key={jogador.nome} nome={jogador.nome} equipe={jogador.equipe} imagem={jogador.imagem}/> )}
+                {props.jogadores.map( jogador => {
+                    console.log('renderizando jogador');
+                    return <Jogador corDeFundo={props.corPrimaria} key={jogador.nome} nome={jogador.nome} equipe={jogador.equipe} imagem={jogador.imagem}/>
+                })}
             </div>
         </section>
         : ''
