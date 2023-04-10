90cd0a7865c03af2c245eee6dfd7e3ca6e82939e
m-4morim <m.4morim@outlook.com>
2023-04-10 20:36:34 -0300

    :heavy_plus_sign: buid: Instalando hex-to-rgb

--- src/componentes/Posicao/index.js
+++ src/componentes/Posicao/index.js
@@ -1,9 +1,10 @@
 import Jogador from '../Jogador'
 import './Posicao.css'
+import hexToRgba from 'hex-to-rgba'
 
 const Posicao = ({ posicao, jogadores, aoDeletar, mudarCor }) => {
 
-    const css = { backgroundColor: posicao.cor }
+    const css = { backgroundColor: hexToRgba(posicao.cor, '0.6') }
     return (
         (jogadores.length > 0) ? <section className='posicao' style={css}>
             <input onChange={evento => mudarCor(evento.target.value, posicao.nome)} value={posicao.cor} type='color' className='input-cor' />
